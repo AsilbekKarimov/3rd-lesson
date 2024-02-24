7709dee5b9f4bc36d5c03f4f46db70c94b3e57ba
AsilbekKarimov <151533521+AsilbekKarimov@users.noreply.github.com>
2024-02-25 01:20:58 +0500

    Add files via upload

---
+++ 3rd lesson/main.js
@@ -0,0 +1,55 @@
+// let ism = prompt("Ismingizni Yozing")
+// let surname = prompt("familyangizni Yozing")
+// let age = prompt("Yoshingizni Yozing")
+
+// if(age > 17){
+//     console.log("Xush Kelibsiz " + ism)
+// } else{
+//     console.log("Maloletka " + ism)
+// }
+
+let ism = prompt("Ismingizni kiriting");
+let surname = prompt("Familyangizni kiriting");
+let age = prompt("Yoshingizni kiriting");
+let univ = prompt("Universitetni tanglang");
+let ball = Math.ceil(Math.random() * 250);
+
+if (ism === "" && surname === "" && age === "" && univ === "" && ball > 180) {
+  console.log("Togri toldiring");
+} else if (ism && surname && age > 17 && univ.toUpperCase && ball > 180) {
+  console.log(
+    `TABRIKLAYMIZ ${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilindingiz`
+  );
+} else if (ism && surname && age > 17 && univ.toUpperCase && ball > 180) {
+  console.log(
+    `TABRIKLAYMIZ ${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilindingiz`
+  );
+} else if (ism && surname && age > 17 && univ.toUpperCase && ball > 180) {
+  console.log(
+    `TABRIKLAYMIZ ${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilindingiz`
+  );
+} else if (ism && surname && age > 17 && univ.toUpperCase && ball > 180) {
+  console.log(
+    `TABRIKLAYMIZ ${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilindingiz`
+  );
+} else if (ism && surname && age > 17 && univ.toUpperCase && ball > 180) {
+  console.log(
+    `TABRIKLAYMIZ ${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilindingiz`
+  );
+} else if (ism && surname && age > 17 && univ.toUpperCase && ball > 180) {
+  console.log(
+    `TABRIKLAYMIZ ${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilindingiz`
+  );
+} else if (ism && surname && age > 17 && univ.toUpperCase && ball > 180) {
+  console.log(
+    `TABRIKLAYMIZ ${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilindingiz`
+  );
+} else if (ism && surname && age > 17 && univ.toUpperCase && ball > 180) {
+  console.log(
+    `TABRIKLAYMIZ ${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilindingiz`
+  );
+} else {
+  console.log(
+    `${ism} ${surname} siz ${ball} ball bilan ${univ} oliygohiga qabul qilinmadingiz`
+  );
+}
